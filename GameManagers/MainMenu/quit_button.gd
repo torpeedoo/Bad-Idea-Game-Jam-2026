35c82065a076e6de7e63766146e9be83cb742a21
Torpeedoo <marckusman@gmail.com>
2026-03-30 19:04:36 -0400

    quit btn

---
+++ GameManagers/MainMenu/quit_button.gd
@@ -0,0 +1,12 @@
+extends MainMenuButton
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if OS.has_feature("web"):
+		disabled = true
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
